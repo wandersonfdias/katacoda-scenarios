4a7bc74260561c2da3359513d41b0357fb33c388
wandersonfdias <wandersonf.dias@gmail.com>
2019-11-03 18:31:07 -0300

    dev

--- teste-wanderson/start-course.sh
+++ teste-wanderson/start-course.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/bin/sh
+
+launch.sh
 
 minikube start
+minikube addons enable dashboard
 
 source <(kubectl completion bash)
 
-{ clear && echo 'Kubernetes is ready.'; } 2> /dev/null
+clear && echo 'Kubernetes is ready.' 2> /dev/null
